e1d77e8e0ae325247baee980c67c94a4950d37f3
d_dao <dung.dao@hotmail.de>
2021-05-05 11:15:09 +0200

    Init commit

---
+++ src/main/java/de/neuefische/orderspringserver/OrderSpringServerApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.orderspringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSpringServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/orderspringserver/OrderSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.orderspringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSpringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

afb09f4dd811b0433c87f0adff1ffcb264cc3db9
d_dao <dung.dao@hotmail.de>
2021-05-05 16:03:31 +0200

    Add OrderController and OrderService

--- src/main/java/de/neuefische/orderspringserver/controller/OrderController.java
+++ src/main/java/de/neuefische/orderspringserver/controller/OrderController.java
@@ -1,12 +1,38 @@
 package de.neuefische.orderspringserver.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.neuefische.orderspringserver.model.Order;
+import de.neuefische.orderspringserver.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("order")
 public class OrderController {
 
-    public OrderController() {}
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public List<Order> listOfOrders() {
+        return orderService.list();
+    }
+
+    @PutMapping("{id}")
+    public Order addOrder(@RequestBody Order newOrder, @PathVariable String id) {
+        if (id.equals(newOrder.getId())) {
+            if (orderService.findOrderById(id).isPresent()) {
+                return orderService.updateOrder(orderService.findOrderById(id).get(), newOrder);
+            }
+            return orderService.addOrder(newOrder);
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID not matching");
+    }
 
 }
--- src/main/java/de/neuefische/orderspringserver/service/OrderService.java
+++ src/main/java/de/neuefische/orderspringserver/service/OrderService.java
@@ -1,4 +1,44 @@
 package de.neuefische.orderspringserver.service;
 
+import de.neuefische.orderspringserver.db.OrderDb;
+import de.neuefische.orderspringserver.db.ProductDb;
+import de.neuefische.orderspringserver.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OrderService {
+
+    private final OrderDb orderDb;
+    private final ProductDb productDb;
+
+    @Autowired
+    public OrderService(OrderDb orderDb, ProductDb productDb) {
+        this.orderDb = orderDb;
+        this.productDb = productDb;
+    }
+
+    public List<Order> list() {
+        return orderDb.list();
+    }
+
+    public Order addOrder(Order order) {
+        //productDb.findProductById(id
+        return orderDb.addOrder(order);
+    }
+
+    public Order removeOrder(Order order) {
+        return orderDb.removeOrder(order);
+    }
+
+    public Order updateOrder(Order orderInDb, Order newOrder) {
+        return orderDb.updateOrder(orderInDb, newOrder);
+    }
+
+    public Optional<Order> findOrderById(String id) {
+        return orderDb.findOrderById(id);
+    }
 }
